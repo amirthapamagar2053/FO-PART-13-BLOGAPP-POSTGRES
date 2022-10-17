13949162e0ac88b8a473f2ee604153a3661f8220
Amir thapa magar <amirthapa2019@gmail.com>
2022-10-17 21:52:00 +0545

    feat:fin 13.13

--- controllers/blogRouter.js
+++ controllers/blogRouter.js
@@ -1,7 +1,7 @@
 const blogRouter = require("express").Router();
 const jwt = require("jsonwebtoken");
 const { SECRET } = require("../util/config");
-
+const { Op } = require("sequelize");
 const { Blog, User } = require("../models");
 
 const tokenExtractor = (req, res, next) => {
@@ -24,12 +24,21 @@ const blogFinder = async (req, res, next) => {
 };
 
 blogRouter.get("/", async (req, res) => {
+  const where = {};
+
+  if (req.query.search) {
+    where.title = {
+      [Op.substring]: req.query.search,
+    };
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userId"] },
     include: {
       model: User,
       attributes: ["name"],
     },
+    where,
   });
   res.json(blogs);
 });
