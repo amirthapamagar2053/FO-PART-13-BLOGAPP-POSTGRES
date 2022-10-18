b4473c62ed8301b7eb20210f24d3b91a20757b7a
Amir thapa magar <amirthapa2019@gmail.com>
2022-10-18 22:56:21 +0545

    feat:fin 13.16 (need to verify)

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const { connectToDatabase } = require("./util/db");
 const blogRouter = require("./controllers/blogRouter");
 const userRouter = require("./controllers/userRouter");
 const loginRouter = require("./controllers/loginRouter");
+const authorRouter = require("./controllers/authorRouter");
 
 app.use(express.json());
 
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/authors", authorRouter);
 
 const start = async () => {
   await connectToDatabase();
